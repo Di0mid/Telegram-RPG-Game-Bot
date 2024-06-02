db3f3c2359a43d3287b346e2e34401ccaf89069e
Di0mid <diomidlol@gmail.com>
2024-06-02 14:49:49 +0300

    Initial commit

---
+++ Telegram RPG Game Bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Telegram_RPG_Game_Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
