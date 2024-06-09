70d7ae3b8bd16f3cde6544dbae41a548a1a96add
Di0mid <diomidlol@gmail.com>
2024-06-09 14:31:22 +0300

    Base inventory system

---
+++ Telegram RPG Game Bot/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Telegram_RPG_Game_Bot;
+
+[JsonObject(MemberSerialization.OptIn)]
+public class Inventory
+{
+    [JsonConstructor]
+    private Inventory(){}
+    
+    [JsonProperty]
+    private List<Item> _inventory = new();
+
+    public bool TryGetItem(string itemName, out Item? item)
+    {
+        item = _inventory.FirstOrDefault(i => i.Name.ToLower().Equals(itemName.ToLower()));
+
+        if (item == null) 
+            return false;
+        
+        _inventory.Remove(item);
+        return true;
+    }
+    
+    public void AddItem(Item item)
+    {
+        _inventory.Add(item);
+    }
+    
+    public string Info()
+    {
+        var info = "";
+        for (var i = 0; i < _inventory.Count; i++)
+        {
+            info += $"\n{i + 1}) {_inventory[i].Name}";
+        }
+        
+        return $"{info}";
+    }
+}
---
+++ Telegram RPG Game Bot/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace Telegram_RPG_Game_Bot;
+
+public abstract class Item
+{
+    public Item(string name)
+    {
+        Name = name;
+    }
+    
+    public string Name { get; private set; }
+
+    public string Info()
+    {
+        return $"*Название*: {Name}";
+    }
+}
